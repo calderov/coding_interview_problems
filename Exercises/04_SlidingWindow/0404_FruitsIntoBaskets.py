cde09212eaa250ef058f5636d8a18ee37d93b7e0
calderov <calderov@gmail.com>
2023-08-28 15:56:54 -0700

    Update PyDocs for S04P04

--- Exercises/04_SlidingWindow/0404_FruitsIntoBaskets.py
+++ Exercises/04_SlidingWindow/0404_FruitsIntoBaskets.py
@@ -29,10 +29,43 @@
 
 class Solution:
     # Solution:
+    # This problem can be stated as finding the longest subarray with at most 2 different
+    # kinds of fruit. To solve it we can proceed as follows:
     #
+    # 1. Initialize a variable to keep track of the length of the longest subarray
+    #    that matches our criteria NumberOfDifferentKindsOfFruit(subarray) <= 2.
+    #   
+    #    Since we have not evaluated any subarray yet, initialize this variable to
+    #    zero.
+    #      maxFruitsIntoBaskets = 0
+    # 
+    # 2. Initialize a couple of pointers to mark the start and the end of a sliding window.
+    #    start = 0
+    #    end = 0
+    #
+    # 3. Initialize an empty list named subarray, we will use it to keep a copy of the
+    #    subarray within the sliding window so it is easy to operate.
+    #
+    # 4. While the end of the sliding window is still within the limits of the input array 
+    #   (fruits) append fruits[end] to the subarray.
+    # 
+    # 5. If the number of distinct fruits in the subarray is less than 2, check if the
+    #    length of the subarray is greater than maxFruitsIntoBaskets, and update it if
+    #    needed.
+    #      maxFruitsIntoBaskets = max(maxFruitsIntoBaskets, len(subarray))
+    #
+    # 6. If the number of distinct characters in the subarray is greater than 2, remove the first
+    #    element of the subarray until this is no longer the case. Do this using a while loop 
+    #    and add 1 to the start pointer on each iteration.
+    #
+    # 7. Add 1 to the end pointer and loop back to step 4, unless the end pointer has passed
+    #    the end of the string.
+    #
+    # 8. Return maxFruitsIntoBaskets and finish.
+    #    
     # Solution complexity:
-    # Time complexity: 
-    # Space complexity: 
+    # Time complexity: O(n)
+    # Space complexity: O(n) as the support subarray can be as long as the original array
     def MaxNumberOfFruitsIntoBaskets(self, fruits):
         maxFruitsIntoBaskets = 0
         subarray = []
