1f2b9a2f9bff99991052dd306656f9cc53bbf9bf
calderov <calderov@gmail.com>
2023-09-07 00:52:18 -0700

    Update merged intervals examples

--- Exercises/05_MergeIntervals/0507_ProblemChallenge3.py
+++ Exercises/05_MergeIntervals/0507_ProblemChallenge3.py
@@ -4,23 +4,19 @@
 # interval which is common to all employees. You can assume that each list of
 # employee working hours is sorted on the start time.
 # 
-# Example 1:
+# Examples:
 # 
-# Input: Employee Working Hours=[[[1,3], [5,6]], [[2,3], [6,8]]]
-# Output: [3,5]
-# Explanation: All the employees are free between [3,5].
+#   Input: Employee Working Hours=[[[1,3], [5,6]], [[2,3], [6,8]]]
+#   Output: [3,5]
+#   Explanation: All the employees are free between [3,5].
 # 
-# Example 2:
+#   Input: Employee Working Hours=[[[1,3], [9,12]], [[2,4]], [[6,8]]]
+#   Output: [4,6], [8,9]
+#   Explanation: All employees are free between [4,6] and [8,9].
 # 
-# Input: Employee Working Hours=[[[1,3], [9,12]], [[2,4]], [[6,8]]]
-# Output: [4,6], [8,9]
-# Explanation: All employees are free between [4,6] and [8,9].
-# 
-# Example 3:
-# 
-# Input: Employee Working Hours=[[[1,3]], [[2,4]], [[3,5], [7,9]]]
-# Output: [5,7]
-# Explanation: All employees are free between [5,7].
+#   Input: Employee Working Hours=[[[1,3]], [[2,4]], [[3,5], [7,9]]]
+#   Output: [5,7]
+#   Explanation: All employees are free between [5,7].
 # 
 
 class Interval:
