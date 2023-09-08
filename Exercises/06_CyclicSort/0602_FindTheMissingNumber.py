d56c01634d71a0896cc9674940e097da839dd940
calderov <calderov@gmail.com>
2023-09-07 17:58:15 -0700

    Explain solutions fro S06P02

--- Exercises/06_CyclicSort/0602_FindTheMissingNumber.py
+++ Exercises/06_CyclicSort/0602_FindTheMissingNumber.py
@@ -15,7 +15,17 @@
 
 class Solution:
     # Solution:
+    # 1. Make a set out of all the numbers from 0 to n where n = len(nums) + 1.
     # 
+    # 2. Make a set ouf of all the numbers in the nums array.
+    #
+    # 3. Compute the difference between the set with all the numbers from 0 to n,
+    #    and the set with all the numbers in nums.
+    # 
+    # 4. The resulting set should have a single member, the missing number.
+    # 
+    # 5. Return the missing number.
+    #
     # Solution complexity:
     # Time complexity: O(n)
     # Space complexity: O(n)
@@ -23,7 +33,23 @@ class Solution:
         return set([i for i in range(len(nums) + 1)]).difference(set(nums)).pop()
 
     # Solution:
-    # 
+    # 1. Initialize an index i = 0.
+    #
+    # 2. While i < len(nums), check if nums[i] < len(nums) and nums[i] != nums[nums[i]]
+    #    this is, check if the number pointed by i in nums is less than the length of nums
+    #    (thus is subject to serve as an index later on), and if the number pointed by i
+    #    in nums is out of its correct place. If this is the case, swap the values at
+    #    nums[i] and nums[nums[i]] and continue. Otherwise, increment i by 1.
+    #
+    # 3. Once the while loop in step 2 is finished, the nums array should be in a
+    #    semisorted state.
+    #
+    #    Use a for loop to traverse the nums array from 0 to len(nums).
+    #    If there is an i such that nums[i] != i, then i is the missing number,
+    #    return it and finish. Otherwise, the array is completely sorted, meaning
+    #    that the missing number is n. Since n = len(nums) by definition, return it
+    #    and finish.  
+    #
     # Solution complexity:
     # Time complexity: O(n)
     # Space complexity: O(1)
