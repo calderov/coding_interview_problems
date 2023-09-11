3dcfc84d58e58aa91840ddb9d948ca688ece4b2c
calderov <calderov@gmail.com>
2023-09-11 11:46:34 -0700

    Remove unnecessary code

--- Exercises/06_CyclicSort/0608_ProblemChallenge3.py
+++ Exercises/06_CyclicSort/0608_ProblemChallenge3.py
@@ -72,15 +72,6 @@ class Solution:
         # Return missing numbesr array
         return missingNumbers
 
-    def CyclicSortExtended(self, nums):
-        i = 0
-        while i < len(nums):
-            if nums[i] < len(nums) and nums[i] > 0 and nums[i] != nums[nums[i] - 1]:
-                j = nums[i] - 1
-                nums[i], nums[j] = nums[j], nums[i]
-                continue
-            i += 1
-
 if __name__ == "__main__":
     solution = Solution()
 
