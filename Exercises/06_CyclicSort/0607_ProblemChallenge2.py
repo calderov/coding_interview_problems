a31eaead99304d85d42abe10697e13427bb7b295
calderov <calderov@gmail.com>
2023-09-11 13:11:14 -0700

    Small bug fix to S06P07

--- Exercises/06_CyclicSort/0607_ProblemChallenge2.py
+++ Exercises/06_CyclicSort/0607_ProblemChallenge2.py
@@ -75,7 +75,7 @@ class Solution:
     def CyclicSortExtended(self, nums):
         i = 0
         while i < len(nums):
-            if nums[i] > 0 and nums[i] < len(nums) and nums[i] != nums[nums[i] - 1]:
+            if nums[i] > 0 and nums[i] <= len(nums) and nums[i] != nums[nums[i] - 1]:
                 j = nums[i] - 1
                 nums[i], nums[j] = nums[j], nums[i] # swap
                 continue
