754a0f73133bb00366ad781f29601f9bb695fe30
calderov <calderov@gmail.com>
2023-09-19 20:32:33 -0700

    Draft 0901

---
+++ Exercises/09_TreeBreadthFirstSearch/0901_BinaryTreeOrderTraversal.py
@@ -0,0 +1,34 @@
+# Binary Tree Level Order Traversal (easy)
+# Problem Statement
+# 
+# Given a binary tree, populate an array to represent its level-by-level
+# traversal. You should populate the values of all nodes of each level from
+# left to right in separate sub-arrays.
+# 
+# Example:
+# 
+#              1
+#             / \  
+#            /   \  
+#           /     \  
+#          /       \  
+#         2         3  
+#        / \       / \
+#       /   \     /   \
+#      4     5   6     7
+#
+# Level order traversal:[[1],[2,3],[4,5,6,7]]
+# 
+class Solution:
+    # Solution:
+    # 
+    # Solution complexity:
+    # Time complexity: 
+    # Space complexity: 
+    def foo(self):
+        pass
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    
