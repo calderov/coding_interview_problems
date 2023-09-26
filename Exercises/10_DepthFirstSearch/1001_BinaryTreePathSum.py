d62f3977ffae15ffefa601a1ebd67bbc7a9fb3c1
calderov <calderov@gmail.com>
2023-09-25 20:46:58 -0700

    Solve 1001 binary tree path sum

---
+++ Exercises/10_DepthFirstSearch/1001_BinaryTreePathSum.py
@@ -0,0 +1,67 @@
+# Problem:
+# Given a binary tree and a number S, find if the tree has a path from
+# root-to-leaf such that the sum of all the node values of that path equals
+# S.
+#
+# Example:
+# 
+#   Input:
+#             ┌─┐
+#        ┌────┤1├────┐
+#        │    └─┘    │
+#       ┌┴┐         ┌┴┐
+#     ┌─┤2├─┐     ┌─┤3├─┐
+#     │ └─┘ │     │ └─┘ │
+#    ┌┴┐   ┌┴┐   ┌┴┐   ┌┴┐
+#    │4│   │5│   │6│   │7│
+#    └─┘   └─┘   └─┘   └─┘
+# 
+#   S = 10
+# 
+# Output: True
+# Explication: 1 + 3 + 6 = 10
+# 
+
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+        self.next = None
+
+class Solution:
+    # Solution:
+    # 
+    # Solution complexity:
+    # Time complexity: 
+    # Space complexity: 
+    def FindSumDFS(self, root, targetSum):
+        if not root:
+            return False
+        
+        if root.val == targetSum and not root.left and not root.right:
+            return True
+        
+        return self.FindSumDFS(root.left, targetSum - root.val) or self.FindSumDFS(root.right, targetSum - root.val)
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Example 1
+    tree = TreeNode(1)
+    tree.left = TreeNode(2)
+    tree.right = TreeNode(3)
+    tree.left.left = TreeNode(4)
+    tree.left.right = TreeNode(5)
+    tree.right.left = TreeNode(6)
+    tree.right.right = TreeNode(7)
+    
+    s = 10 
+
+    expectedOutput = True
+    output = solution.FindSumDFS(tree, s)
+
+    print(output)
+    print(expectedOutput)
+    print(output == expectedOutput)
+    print()
