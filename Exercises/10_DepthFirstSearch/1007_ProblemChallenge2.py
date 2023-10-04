12849242f1c7027adb6543ab18f4e9d4790befed
calderov <calderov@gmail.com>
2023-10-03 20:02:01 -0700

    Comment solutions on 1006 and 1007

--- Exercises/10_DepthFirstSearch/1007_ProblemChallenge2.py
+++ Exercises/10_DepthFirstSearch/1007_ProblemChallenge2.py
@@ -39,7 +39,12 @@ class TreeNode:
 
 class Solution:
     # Solution:
-    # 
+    # This problem follows the Binary Tree Path Sum pattern and shares the
+    # algorithmic logic with Tree Diameter. We can follow the same DFS approach.
+    # The only difference will be to ignore the paths with negative sums. Since
+    # we need to find the overall maximum sum, we should ignore any path which
+    # has an overall negative sum.
+    #
     # Time complexity: O(n log(n))
     # Space complexity: O(n)
     def MaximumPathSum(self, root):
