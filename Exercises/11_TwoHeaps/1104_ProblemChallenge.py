3f72569144ffca96eec80c975fe9a53ee99432bd
calderov <calderov@gmail.com>
2023-10-05 15:34:22 -0700

    Comment 1104

--- Exercises/11_TwoHeaps/1104_ProblemChallenge.py
+++ Exercises/11_TwoHeaps/1104_ProblemChallenge.py
@@ -28,34 +28,72 @@ class Interval:
 
 class Solution:
     # Solution:
-    # 
+    # 1. Initialize results array.
+    #    nextIntervals = [-1] * n
+    #
+    # 2. Initialize max heaps to track start times, end times and their corresponding indexes.
+    #    maxStart = []
+    #    maxEnd = []
+    #
+    # 3. Populate maxStart and maxEnd heaps.
+    #
+    # 4. Go through all the intervals to find each interval's next interval.
+    #
+    #    4.1 Let i be the index of the interval with the latest end time in the maxEnd heap.
+    #
+    #    4.2 If interval[i] ends before than, or at the same time as the interval with the latest
+    #        start date in maxStart (lets call it interval[j]), then interval[j] is a candidate
+    #        to be the next interval after interval[i].
+    #
+    #       4.2.1 Loop through the elements in maxStart to see if there is a better candidate.
+    #
+    #       4.2.2 Register the best j as the next interval.
+    #
+    #       4.2.3 Put the interval[j] back in the maxStart heap as it could be the next interval of
+    #             another interval.
+    #
+    #       4.2.4 Return the list of next intervals and finish.
+    #
     # Solution complexity:
     # Time complexity: O(n log(n))
     # Space complexity: O(n)
     def NextIntervals(self, intervals: list[Interval]):
         n = len(intervals)
         
+        # Initialize results array
         nextIntervals = [-1] * n
+
+        # Initialize max heaps to track start times, end times
+        # and their corresponding indexes
         maxStart = []
         maxEnd = []
 
+        # Populate maxStart and maxEnd heaps
         for i in range(n):
             heappush(maxStart, (-intervals[i].start, i))
             heappush(maxEnd, (-intervals[i].end, i))
 
+        # Go through all the intervals to find each interval's next interval
         for _ in range(n):
+            # Let i be the index of the interval with the latest end time in the maxEnd heap
             i_end, i = heappop(maxEnd)
 
+            # If interval[i] ends before than, or at the same time as the interval with the latest
+            # start date in maxStart (lets call it interval[j]), then interval[j] is a candidate
+            # to be the next interval after interval[i]
             if -i_end <= -maxStart[0][0]:
+                # Loop through the elements in maxStart to see if there is a better candidate
                 j_start, j = heappop(maxStart)
-
                 while maxStart and -i_end <= -maxStart[0][0]:
                    j_start, j = heappop(maxStart)
-
+                # Register the best j as the next interval
                 nextIntervals[i] = j
 
+                # Put the interval[j] back in the maxStart heap as it could be the next interval of
+                # another interval
                 heappush(maxStart, (j_start, j))
 
+        # Return the list of next intervals and finish
         return nextIntervals
 
 def ListOfListsToIntervalsList(listOfLists):
