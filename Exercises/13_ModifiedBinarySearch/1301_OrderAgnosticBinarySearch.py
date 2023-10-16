afe1e99106c7ada726809a8efd7c672cd7d8aa29
calderov <calderov@gmail.com>
2023-10-16 16:48:14 -0700

    Comment solution for 1301

--- Exercises/13_ModifiedBinarySearch/1301_OrderAgnosticBinarySearch.py
+++ Exercises/13_ModifiedBinarySearch/1301_OrderAgnosticBinarySearch.py
@@ -24,6 +24,38 @@
 
 class Solution:
     # Solution:
+    # 1. Initialize 'left' and 'right' pointers.
+    #    left = 0
+    #    right = n - 1, where n is the length of the given array.
+    #
+    # 2. Check if the array is sorted in ascending order (or not).
+    #    ascending = nums[left] < nums[rigth]
+    #
+    # 3. While the 'left' pointer is less than or equal to the 'right' pointer.
+    #
+    #   3.1 Compute the 'middle' pointer (that at the middle of the search window).
+    #   
+    #   3.2 If the item at the 'middle' is our desired key, return the 'middle' pointer.
+    #
+    #   3.3 If the item at the 'middle' is less than our desired key.
+    #
+    #     3.3.1 If the input array is sorted in ascending order, move the 'left' pointer
+    #           one position to the right of the 'middle' pointer.
+    #
+    #     3.3.2 Otherwise, move the 'right' pointer one position to the left of the 'middle'
+    #           pointer.
+    #
+    #   3.4 If the item at the 'middle' is greater than our desired key.
+    #
+    #     3.4.1 If the input array is sorted in ascending order, move the 'right' pointer
+    #           one position to the left of the 'middle' pointer.
+    #
+    #     3.4.2 Otherwise, move the 'left' pointer one position to the right of the 'middle' pointer.
+    #
+    #   3.5 Repeat step 3 until 'left' == 'right'.
+    #
+    # 4. If the algorithm did not return anything on step 3 it means that the given key is not in the input
+    #    array. If this is the case, return -1 and finish.
     # 
     # Solution complexity:
     # Time complexity: 
@@ -32,26 +64,42 @@ class Solution:
         if not nums:
             return -1
 
+        # Initialize 'left' and 'right' pointers
         left = 0
         right = len(nums) - 1
+
+        # Check if the array is sorted in ascending order (or not)
         ascending = nums[left] < nums[right]
 
+        # While the 'left' pointer is less than or equal to the 'right' pointer
         while left <= right:
+            # Compute the middle pointer (that at the middle of the search window)
             mid = (right + left) // 2
 
+            # If the item at the 'middle' is our desired key, return the 'middle' pointer
             if nums[mid] == key:
                 return mid
 
+            # If the item at the 'middle' is less than our desired key
             if nums[mid] < key:
+                # If the input array is sorted in ascending order, move the 'left' pointer
+                # one position to the right of the 'middle' pointer
                 if ascending: left = mid + 1
+                # Otherwise, move the 'right' pointer one position to the left of the
+                # 'middle' pointer
                 else: right = mid - 1
                 continue
 
+            # If the item at the 'middle' is greater than our desired key
             if nums[mid] > key:
+                # If the input array is sorted in ascending order, move the 'right' pointer
+                # one position to the left of the 'middle' pointer
                 if ascending: right = mid - 1
+                # Otherwise, move the 'left' pointer one position to the right of the 'middle' pointer
                 else: left = mid + 1
                 continue
-            
+        
+        # Return -1 as the key given has not been found on the array up to this point
         return -1
 
 if __name__ == "__main__":
