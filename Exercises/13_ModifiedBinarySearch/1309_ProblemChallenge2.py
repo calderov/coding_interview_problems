4f4ba91b914993d72a4c2425f0a5a69fd7b85d76
calderov <calderov@gmail.com>
2023-10-19 17:08:59 -0700

    Comment on 1309

--- Exercises/13_ModifiedBinarySearch/1309_ProblemChallenge2.py
+++ Exercises/13_ModifiedBinarySearch/1309_ProblemChallenge2.py
@@ -19,10 +19,22 @@
 
 class Solution:
     # Solution:
-    # 
+    # 1. Find the rotation point (that point i where nums[i] > nums[i + 1])
+    #
+    # 2. Search key in first half (nums[0 : rotationPoint + 1]). If the key is present, 
+    #    return its position and finish. Otherwise proceed to the next step.
+    #
+    # 3. Search key in second half (nums[rotationPoint + 1 : len(nums)]). If the key is present,
+    #    add rotationPoint + 1 to its position and return it.
+    #
+    # 4. Return -1 as the key was not found in neither one of both halves.
+    #
     # Solution complexity:
-    # Time complexity: 
-    # Space complexity: 
+    # Time complexity: O(n)   // Note: As it is implemented here, finding the rotation point takes O(n) steps.
+    #                                  Thus the overall complexity of this algorithm is O(n). This problem
+    #                                  can be solved in O(log(n)) but I due to time constraints I will leave
+    #                                  this solution as is.
+    # Space complexity: O(1)
     def FindKeyInRotatedArray(self, nums, key):
         # Find rotation point
         rotationPoint = self.FindRotationPosition(nums)
