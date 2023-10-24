784d53c5812a9e8b6cbdc8bbc5a7e2fc62fce93d
calderov <calderov@gmail.com>
2023-10-24 15:47:23 -0700

    Solve 1507 kth largest number in stream

---
+++ Exercises/15_TopKElements/1507_KthLargestNumberInStream.py
@@ -0,0 +1,79 @@
+# Problem Statement
+# 
+# Design a class to efficiently find the Kth largest element in a stream of
+# numbers.
+# 
+# The class should have the following two things:
+# 
+#     The constructor of the class should accept an integer array containing
+# initial numbers from the stream and an integer ‘K’.
+#     The class should expose a function add(int num) which will store the
+# given number and return the Kth largest number.
+# 
+# Example:
+# 
+#   Input: [3, 1, 5, 12, 2, 11], K = 4
+#   1. Calling add(6) should return '5'.
+#   2. Calling add(13) should return '6'.
+#   2. Calling add(4) should still return '6'.
+# 
+
+from heapq import *
+
+class Solution:
+    # Solution:
+    # Use a max heap to store the stream of numbers.
+    # 
+    # Each time a new number is added, pop k - 1 elements from the heap and store
+    # them into a temporary list. Then, copy the top item of the heap to a variable
+    # named kthlargest (as the top item of the heap is the kth largest item at the moment),
+    # and push all of the items in the temporary list back into the max heap.
+    # Finally, return kthLargest and finish.
+    #
+    #
+    # Solution complexity:
+    # Time complexity: O(n log(k))
+    # Space complexity: O(n)
+    def __init__(self, nums, k):
+        self.maxHeap = []
+        self.k = k
+        
+        for num in nums:
+            heappush(self.maxHeap, -num)
+
+    def add(self, num):
+        heappush(self.maxHeap, -num)
+
+        temp = []
+        for i in range(self.k - 1):
+            temp.append(heappop(self.maxHeap))
+
+        kthLargest = -self.maxHeap[0]
+
+        while temp:
+            heappush(self.maxHeap, temp.pop())
+
+        return kthLargest
+
+if __name__ == "__main__":
+    # Example 1
+    nums = [3, 1, 5, 12, 2, 11]
+    k = 4
+
+    solution = Solution(nums, k)
+
+    # 1. Calling add(6) should return '5'.
+    expectedOutput = 5
+    output = solution.add(6)
+    print(output, expectedOutput, output == expectedOutput)
+
+    # 2. Calling add(13) should return '6'.
+    expectedOutput = 6
+    output = solution.add(13)
+    print(output, expectedOutput, output == expectedOutput)
+
+    # 3. Calling add(4) should still return '6'.
+    expectedOutput = 6
+    output = solution.add(4)
+    print(output, expectedOutput, output == expectedOutput)
+
