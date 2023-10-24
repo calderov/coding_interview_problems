153f4f5f529b951df7a495d297d42d0230fc7178
calderov <calderov@gmail.com>
2023-10-23 17:30:57 -0700

    Solve 1401 single number

---
+++ Exercises/14_BitwiseXOR/1401_SingleNumber.py
@@ -0,0 +1,42 @@
+# Problem:
+# In a non-empty array of integers, every number appears twice except for
+# one, find that single number.
+# 
+# Examples:
+# 
+#   Input: 1, 4, 2, 1, 3, 2, 3
+#   Output: 4
+# 
+#   Input: 7, 9, 7
+#   Output: 9
+# 
+
+class Solution:
+    # Solution:
+    # 
+    # Solution complexity:
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def FindSingleNumber(self, nums):
+        n = len(nums)
+        
+        single = nums[0]
+        for i in range(1, n):
+            single = single ^ nums[i]
+
+        return single
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Example 1:
+    nums = [1, 4, 2, 1, 3, 2, 3]
+    expectedOutput = 4
+    output = solution.FindSingleNumber(nums)
+    print(output, expectedOutput, output == expectedOutput)
+
+    # Example 2:
+    nums = [7, 9, 7]
+    expectedOutput = 9
+    output = solution.FindSingleNumber(nums)
+    print(output, expectedOutput, output == expectedOutput)
