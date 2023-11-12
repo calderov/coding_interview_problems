d78b7f2395044e98bbc88faad235d24a9002e7cb
Arturo Calderon <calderov@gmail.com>
2023-11-12 05:10:41 -0800

    Comment 1706

--- Exercises/17_KnapsackDynamicProgramming/1706_ProblemChallenge2.py
+++ Exercises/17_KnapsackDynamicProgramming/1706_ProblemChallenge2.py
@@ -26,6 +26,30 @@
 #               -1 + 2 + 7 + 1 = 9
 
 class Solution:
+    # Solution:
+    # This problem can be rephrased as "count the number of partitions P = P1 + P2 of the input array such that sum(P1) - sum(P2) = S".
+    # In other words, the problem is asking us to find how to split the input into sets such that the sum of the members of one these
+    # subsets equals S when we substract the sum of the members of the other subset.
+    #
+    # We can simplify this even further by considering the fact that sum(P1) + sum(P2) = T where T is the total sum of the elements in the
+    # input array. Now, by adding both equations we get:
+    #
+    # (sum(P1) + sum(P2)) + (sum(P1) - sum(P2)) = T + S
+    #
+    # sum(P1) + sum(P2) + sum(P1) - sum(P2) = T + S
+    #
+    # 2 * sum(P1) = T + S
+    #
+    # sum(P1) = (T + S) / 2
+    #
+    # Thus, solving this last equation will answer to the original problem.
+    # 
+    # To do so, we can use dynamic programming to build a table dp where dp[i][j] keeps the number of subsets of the first i elements of
+    # the input, that add up to the value j. This value j ranges from 0 to (T + S) / 2.
+    #
+    # Once the table is populated, return the value stored in the last cell of the table, as it contains how many subsets add to (T + S) / 2.
+    # Or in the original terms, how many subset partitions are equal to S when the sum of one of the subsets is substracted from the sum of
+    # the other subset.
     def CountOfSubsetsAddingToSByModifyingOperators(self, nums, s):
         # Return early if the problem can not be solved by this approach
         if (sum(nums) + s) % 2 != 0:
