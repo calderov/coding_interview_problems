9c72caea2a1d431b512f6e4ae45e7155ecba6655
Arturo Calderon <calderov@gmail.com>
2023-11-12 12:44:26 -0800

    Solve 1801 using backtracking

--- Exercises/18_Backtracking/1801_CombinationSum.py
+++ Exercises/18_Backtracking/1801_CombinationSum.py
@@ -20,13 +20,51 @@
 # Output: [[2,2,2,2,2], [2,2,2,4], [2,2,6], [2,4,4], [2,8], [4,6]]
 # Explanation: The elements in these six combinations sum up to 10.
 
+# Solution:
+# Start with an empty list of results and an empty combination.
+#
+# Iterate through the candidates array, adding each candidate to the current
+# combination and recursively call the function with the updated combination
+# and remaining target.
+#
+# If the target becomes 0, we add the current combination to the result list.
+# If the target becomes negative, we backtrack and remove the last added candidate
+# from the combination.
+#
+# Once this finishes, return the results list.
+#
+# Time complexity: O(n ^ (t / m + 1)) where: n is the number of elements in the input, 
+#                                            t is the target number, 
+#                                            m is the smallest number in the input
+# Space complexity: O(t / n)
 class Solution:
-    # Solution:
-    #
-    # Time complexity: O()
-    # Space complexity: O()
     def CombinationSum(self, candidates, target):
-        pass
+        result = []
+        self.Backtrack(candidates, target, 0, [], result)
+        return result
+    
+    def Backtrack(self, candidates, target, start, combination, result):
+        # If the target is 0, we found a valid combination,
+        # append a copy of this combination to the result
+        if target == 0:
+            result.append(list(combination)) # Cast to list to ensure a new copy
+            return
+        
+        # Iterate through the candidates array, starting from the given index
+        for i in range(start, len(candidates)):
+            # If the current candidate is greater than the remaining target, 
+            # move on to the next candidate
+            if candidates[i] > target:
+                continue
+
+            # Otherwise, add the candidate to the current combination
+            combination.append(candidates[i])
+
+            # Recursively call the function with the updated combination and remaining target
+            self.Backtrack(candidates, target - candidates[i], i, combination, result)
+
+            # Backtrack by removing the last added candidate from the combination
+            combination.pop()
 
 def AreEqual(list1, list2):
     if len(list1) != len(list2):
