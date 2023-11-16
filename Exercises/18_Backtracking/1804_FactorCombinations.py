fb277cee423c1e176b2b14aa932b7e873ee6f6e3
Arturo Calderon <calderov@gmail.com>
2023-11-16 06:50:58 -0800

    1804 factor combinations

--- Exercises/18_Backtracking/1804_FactorCombinations.py
+++ Exercises/18_Backtracking/1804_FactorCombinations.py
@@ -18,11 +18,40 @@
 # Output: [[2, 2, 5], [2, 10], [4, 5]]
 
 class Solution:
+    # Solution:
+    # 1. Given a target number to factor (n), a list of candidate factors (candidates), the current
+    #    product of such candidates (candidatesProduct) and a list to hold the results (results).
+    #    Pass them as arguments to the FindFactors function.
+    #
+    # 2. In The FindFactors function, iterate over all the integers from 2 to n, considering each number i
+    #    as a candidate factor of n if n % i == 0.
+    #
+    # 3. Append this number i to the list of candidates and update the candidatesProduct to account for i:
+    #
+    #    candidatesProduct = candidatesProduct * i
+    #
+    # 4. If the candidatesProduct becomes greater than n, remove i from the candidates list and return.
+    #
+    # 5. If the candidatesProduct is equal to n, we have found that our candidates list holds a valid set of factors of n,
+    #    add a copy of the candidates list to the results (take care so the results list does not contain duplicate
+    #    combinations).
+    #
+    # 6. Recursively, call FindFactors with the updated parameters for candidates, candidatesProduct and results.
+    #    This will make it propose new candidates.
+    #
+    # 7. Backtrack, by dividing the candidates product by i, and removing i from the candidates list (just pop the last
+    #    item of the list).
+    #
+    # 8. Increase i by 1 and iterate once again.
+    #
+    # Solution complexity:
+    # Time complexity: O(n ^ 2)
+    # Space complexity: O(log (n)) Ignoring the space required for the results list.
     def FactorCombinations(self, n):
         results = []
         self.FindFactors(n, [1], 1, results)
         return results
-        
+
     def FindFactors(self, n, candidates, candidatesProduct, results):
         i = 2
         while i < n:
@@ -32,7 +61,7 @@ class Solution:
 
             candidatesProduct *= i
             candidates.append(i)
-            
+
             if candidatesProduct > n:
                 candidates.pop()
                 return
@@ -46,7 +75,7 @@ class Solution:
 
             candidatesProduct //= i
             candidates.pop()
-            
+
             i += 1
 
 if __name__ == "__main__":
