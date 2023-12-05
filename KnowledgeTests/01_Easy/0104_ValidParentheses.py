a37ce5f379c247e6485c1fd79ceaff1a24b02ee3
Arturo Calderon <calderov@gmail.com>
2023-12-05 10:46:22 -0800

    Solve knowledge test 0104 valid parentheses

--- KnowledgeTests/01_Easy/0104_ValidParentheses.py
+++ KnowledgeTests/01_Easy/0104_ValidParentheses.py
@@ -1,23 +1,23 @@
 # Problem:
 # Determine if an input string containing only the characters '(', ')', '{',
 # '}', '[', and ']' is valid. A string is considered valid if:
-# 
+#
 # 1. Open brackets must be closed by the same type of brackets.
 # 2. Open brackets must be closed in the correct order.
 # 3. Each close bracket has a corresponding open bracket of the same type.
 #
 # Examples
-# 
+#
 #   Input: "(]"
 #   Expected Output: false
 #   Justification: The opening parenthesis '(' is not closed by its
 #                  corresponding closing parenthesis.
-# 
+#
 #   Input: "{[]}"
 #   Expected Output: true
 #   Justification: The string contains pairs of opening and closing brackets in
 #                  the correct order.
-# 
+#
 #   Input: "[{]}"
 #   Expected Output: false
 #   Justification: The opening square bracket '[' is closed by a curly brace
@@ -39,18 +39,16 @@ class Solution:
             character = inputString[i]
 
             if character in openingCharacters:
-                stack.append(closingCharacters[character])
+                stack.append(character)
                 continue
-            
-            if not stack:
-                return False
-            
-            if stack[-1] == character:
+
+            if stack and character == closingCharacters[stack[-1]]:
                 stack.pop()
+                continue
             else:
                 return False
 
-        return len(stack) == 0
+        return not stack
 
 if __name__ == "__main__":
     solution = Solution()
