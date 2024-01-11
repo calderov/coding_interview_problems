83671197a3b6427f8171c4fed4a3b8040c495e03
Arturo Calderon <calderov@gmail.com>
2024-01-11 09:27:01 -0800

    Update cyclic sort demo

--- Demos/02_CyclicSort_Demo.py
+++ Demos/02_CyclicSort_Demo.py
@@ -7,6 +7,22 @@ def CyclicSort(nums):
             continue
         i += 1
 
+# This version of the algorithm allows numbers outside the [1, n] range (negatives, 
+# zero and numbers greater than n).
+#
+# Time complexity: O(n)
+# Space complexity: O(1)
+def CyclicSortExtended(self, nums):
+    i = 0
+    while i < len(nums):
+        if nums[i] > 0 and \
+           nums[i] <= len(nums) and \
+           nums[i] != nums[nums[i] - 1]:
+            j = nums[i] - 1
+            nums[i], nums[j] = nums[j], nums[i]
+            continue
+        i += 1
+
 if __name__ == "__main__":
     nums = [2, 1, 4, 5, 3]
     print(nums)
