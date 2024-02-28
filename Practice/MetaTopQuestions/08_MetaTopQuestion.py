906541135f215b3da1b1f1d8c6a0eb1e9e20c2a0
Arturo Calderon <calderov@gmail.com>
2024-02-28 09:27:18 -0800

    Meta top question 08

---
+++ Practice/MetaTopQuestions/08_MetaTopQuestion.py
@@ -0,0 +1,62 @@
+# 1570. Dot Product of Two Sparse Vectors (Medium)
+# Given two sparse vectors, compute their dot product.
+# 
+# Implement class SparseVector:
+#   - SparseVector(nums) Initializes the object with the vector nums
+#   - dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+#
+# A sparse vector is a vector that has mostly zero values, you should store
+# the sparse vector efficiently and compute the dot product between two
+# SparseVector.
+# 
+# Example 1:
+#   Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+#   Output: 8
+#   Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+#                v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+#
+# Example 2:
+#   Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+#   Output: 0
+#   Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+#                v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+# 
+# Example 3:
+#   Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+#   Output: 6
+
+class SparseVector:
+    def __init__(self, nums):
+        self.innerVector = {}
+        for i in range(len(nums)):
+            if nums[i]:
+                self.innerVector[i] = nums[i]
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        result = 0
+        for i in filter(lambda x: x in vec.innerVector, self.innerVector):
+            result += self.innerVector[i] * vec.innerVector[i]
+        return result
+
+if __name__ == "__main__":
+    # Example 1
+    v1 = SparseVector([1,0,0,2,3])
+    v2 = SparseVector([0,3,0,4,0])
+    expectedOutput = 8
+    output = v1.dotProduct(v2)
+    print(output, expectedOutput, output == expectedOutput)
+
+    # Example 2
+    v1 = SparseVector([0,1,0,0,0])
+    v2 = SparseVector([0,0,0,0,2])
+    expectedOutput = 0
+    output = v1.dotProduct(v2)
+    print(output, expectedOutput, output == expectedOutput)
+
+    # Example 3
+    v1 = SparseVector([0,1,0,0,2,0,0])
+    v2 = SparseVector([1,0,0,0,3,0,4])
+    expectedOutput = 6
+    output = v1.dotProduct(v2)
+    print(output, expectedOutput, output == expectedOutput)
