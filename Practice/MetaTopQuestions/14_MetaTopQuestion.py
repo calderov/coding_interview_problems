fe685f382618dcaa897189f13b18538ef5a3d6fc
Arturo Calderon <calderov@gmail.com>
2024-03-01 12:02:07 -0800

    Meta top question 14

--- Practice/MetaTopQuestions/14_MetaTopQuestion.py
+++ Practice/MetaTopQuestions/14_MetaTopQuestion.py
@@ -8,14 +8,7 @@
 # - double next(int val) Returns the moving average of the last size values
 # of the stream.
 #  
-# Example 1:
-#     Input
-#     ["MovingAverage", "next", "next", "next", "next"]
-#     [[3], [1], [10], [3], [5]]
-#     Output
-#     [null, 1.0, 5.5, 4.66667, 6.0]
-# 
-#     Explanation
+# Example:
 #     MovingAverage movingAverage = new MovingAverage(3);
 #     movingAverage.next(1); // return 1.0 = 1 / 1
 #     movingAverage.next(10); // return 5.5 = (1 + 10) / 2
@@ -27,3 +20,32 @@
 # - 1 <= size <= 1000
 # - -105 <= val <= 105
 # - At most 104 calls will be made to next.
+
+from collections import deque
+class MovingAverage:
+    def __init__(self, windowSize):
+        self.window = deque()
+        self.windowSize = windowSize
+        self.sum = 0
+
+    def next(self, val: int) -> float:
+        if len(self.window) == self.windowSize:
+            self.window.append(val)
+            self.sum += val
+            self.sum -= self.window.popleft()
+        
+        elif len(self.window) < self.windowSize:
+            self.window.append(val)
+            self.sum = sum(self.window)
+        
+        else: # len(self.windo) > self.windowSize
+            raise Exception('Window size exceeded')
+
+        return self.sum / len(self.window)
+
+if __name__ == "__main__":
+    movingAverage = MovingAverage(3)
+    print("%.2f %.2f" % (movingAverage.next(1), 1.0))
+    print("%.2f %.2f" % (movingAverage.next(10), 5.5))
+    print("%.2f %.2f" % (movingAverage.next(3), 4.66667))
+    print("%.2f %.2f" % (movingAverage.next(5), 6.0))
