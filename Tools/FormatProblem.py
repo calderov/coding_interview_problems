345ba2b940963730756875d49a183f9bade0472f
Arturo Calderon <calderov@gmail.com>
2024-06-27 14:32:00 -0700

    Bug fix on FormatProblem logic

--- Tools/FormatProblem.py
+++ Tools/FormatProblem.py
@@ -10,11 +10,11 @@ def format_text_to_python_comment_of_80_cols(text):
             formattedLines.append("# " + line + "\n")
             continue
 
-        words = line.split(' ')
+        words = line.split(" ")
         newLine = "#"
         while words:
-            word = words.pop(0)
-            if len(newLine) + len(word) + 1 < 78:
+            word = words.pop(0) 
+            if len(word) > 78 or len(newLine) + len(word) + 1 < 78:
                 newLine = newLine + " " + word
             else:
                 formattedLines.append(newLine + "\n")
@@ -23,10 +23,11 @@ def format_text_to_python_comment_of_80_cols(text):
         
         formattedLines.append(newLine + "\n")
     
-    return ''.join(formattedLines)
+    return "".join(formattedLines)
 
 if __name__ == "__main__":
     text = clipboard.paste()
+    text = text.replace("\r", "")
     formatted_text = format_text_to_python_comment_of_80_cols(text)
     print(formatted_text)
     clipboard.copy(formatted_text)
