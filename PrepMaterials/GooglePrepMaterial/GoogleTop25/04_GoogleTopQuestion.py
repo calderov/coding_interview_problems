2103db426eb020a7f5b7822372f5d9b5ddc6ceb0
calderov <calderov@gmail.com>
2026-04-08 23:01:44 -0600

    Format implementation

--- PrepMaterials/GooglePrepMaterial/GoogleTop25/04_GoogleTopQuestion.py
+++ PrepMaterials/GooglePrepMaterial/GoogleTop25/04_GoogleTopQuestion.py
@@ -88,18 +88,18 @@ def GetMedianBest(nums1, nums2):
         i = (lo + hi) // 2
         j = (m + n + 1) // 2 - i
 
-        left1 =  nums1[i - 1] if i > 0 else -inf
+        left1  = nums1[i - 1] if i > 0 else -inf
         right1 = nums1[i]     if i < m else inf
-        left2 =  nums2[j - 1] if j > 0 else -inf
+        left2  = nums2[j - 1] if j > 0 else -inf
         right2 = nums2[j]     if j < n else inf
 
-        # Check if partitions are correct
+        # Verify partitions
         if left1 <= right2 and left2 <= right1:
             if (m + n) % 2 == 1:
                 return max(left1, left2)
             return (max(left1, left2) + min(right1, right2)) / 2
-        
-        # Partitions were not correct, partition again
+
+        # Partitions were wrong, update them
         if left1 > right2:
             hi = i - 1
         else:
@@ -107,6 +107,7 @@ def GetMedianBest(nums1, nums2):
 
     # Unreachable if nums1 and nums2 are sorted
     return None
+
 
 def GetMedian(nums1, nums2):
     return GetMedianBest(nums1, nums2)
