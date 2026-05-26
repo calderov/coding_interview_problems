b7fc963edaf9e48c0c939d2679bd3179a9175d8b
calderov <calderov@gmail.com>
2026-05-26 15:04:02 -0600

    N Queens II

---
+++ PrepMaterials/GooglePrepMaterial/GoogleTop25(Again)/19_GoogleTopQuestionR2.py
@@ -0,0 +1,112 @@
+# N-Queens II
+# HARD
+# https://scaleengineer.com/dsa/problems/n-queens-ii
+
+# Description
+# The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack each other.
+# Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+
+# Example 1:
+# Input: n = 4
+# Output: 2
+# Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+# □ ♔ □ ■
+# ■ □ ■ ♔
+# ♔ ■ □ ■
+# ■ □ ♔ □
+# {(0, 1), (2, 0), (3, 2), (1, 3)}
+
+# □ ■ ♔ ■
+# ♔ □ ■ □
+# □ ■ □ ♔
+# ■ ♔ ■ □
+# {(3, 1), (2, 3), (0, 2), (1, 0)}
+
+# Example 2:
+# Input: n = 1
+# Output: 1
+
+# Constraints:
+#     1 <= n <= 9
+
+class Solution:
+    def __init__(self):
+        self.n = 0
+        self.count = 0
+        self.queens = []
+
+    def totalNQueens(self, n):
+        self.n = n
+        self.count = 0
+        self.queens = []
+        self.backtrack(0)
+        return self.count
+    
+    def backtrack(self, row):
+        if row == self.n:
+            self.count += 1
+            return
+        
+        for col in range(self.n):
+            # Place queen
+            self.queens.append((row, col))
+
+            # Recurse to the next row if the current placement of queens is valid
+            if self.isValid():
+                self.backtrack(row + 1)
+
+            # Backtrack (remove queen)
+            self.queens.pop()
+
+        return
+    
+    def isValid(self):
+        rows = set()
+        cols = set()
+        diag1 = set()
+        diag2 = set()
+
+        for row, col in self.queens:
+            if row in rows or \
+               col in cols or \
+               row + col in diag1 or \
+               row - col in diag2:
+                return False
+            
+            rows.add(row)
+            cols.add(col)
+            diag1.add(row + col)
+            diag2.add(row - col)
+
+        return True
+
+
+if __name__=="__main__":
+    app = Solution()
+    
+    # Example 1:
+    n = 4
+    expected = 2
+    output = app.totalNQueens(n)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+    
+    # Example 2:
+    n = 1
+    expected = 1
+    output = app.totalNQueens(n)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+
+    # Example 3:
+    n = 5
+    expected = 10
+    output = app.totalNQueens(n)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
