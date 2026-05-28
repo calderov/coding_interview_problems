f0bd969ed19bcbb6e52fd2743a6e5e1c6ddfba9b
calderov <calderov@gmail.com>
2026-05-28 11:50:29 -0600

    Insert Interval

---
+++ PrepMaterials/GooglePrepMaterial/GoogleTop25(Again)/22_GoogleTopQuestionR2.py
@@ -0,0 +1,162 @@
+# Insert Interval
+# MEDIUM
+# https://scaleengineer.com/dsa/problems/insert-interval
+
+# Description
+# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end
+# of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end]
+# that represents the start and end of another interval.
+
+# Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have
+# any overlapping intervals (merge overlapping intervals if necessary).
+
+# Return intervals after the insertion.
+
+# Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+# Example 1:
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+
+# Example 2:
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+
+# Constraints:
+#  0 <= intervals.length <= 10**4
+#  intervals[i].length == 2
+#  0 <= starti <= endi <= 10**5
+#  intervals is sorted by starti in ascending order.
+#  newInterval.length == 2
+#  0 <= start <= end <= 10**5
+
+from collections import deque
+
+# Time: O(n)
+# Space: O(n)
+def InsertIntervalCopy(intervals, newInterval):
+    if not intervals:
+        return [[newInterval]]
+    
+    if newInterval[1] < intervals[0][0]:
+        return [[newInterval]] + intervals
+    
+    if intervals[-1][1] < newInterval[0]:
+        return intervals + [[newInterval]]
+    
+    result = []
+    for interval in intervals:
+        if isOverlap(interval, newInterval):
+            if result and result[-1] == newInterval:
+                result.pop()
+            newInterval = merge(interval, newInterval)
+            result.append(newInterval)
+        else:
+            result.append(interval)
+    
+    return result
+
+# Time: O(n)
+# Space: O(1)
+def InsertIntervalInPlace(intervals, newInterval):
+    if not intervals:
+        return [[newInterval]]
+
+    if newInterval[1] < intervals[0][0]:
+        return [[newInterval]] + intervals
+
+    if intervals[-1][1] < newInterval[0]:
+        return intervals + [[newInterval]]
+    
+    intervals.reverse()
+    queue = deque()
+
+    while intervals:
+        interval = intervals.pop()
+        if isOverlap(interval, newInterval):
+            if queue and queue[-1] == newInterval:
+                queue.pop()
+            newInterval = merge(interval, newInterval)
+            queue.append(newInterval)
+        else:
+            queue.append(interval)
+
+    while queue:
+        intervals.append(queue.popleft())
+
+    return intervals
+
+def InsertInterval(intervals, newInterval):
+    return InsertIntervalInPlace(intervals, newInterval)
+
+def isOverlap(interval1, interval2):
+    start1, end1 = interval1
+    start2, end2 = interval2
+    return not (end1 < start2 or end2 < start1)
+
+def merge(interval1, interval2):
+    start1, end1 = interval1
+    start2, end2 = interval2
+    return [min(start1, start2), max(end1, end2)]
+
+if __name__ == "__main__":
+    # Example 1:
+    intervals = [[1,3],[6,9]]
+    newInterval = [2,5]
+    expected = [[1,5],[6,9]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+
+    # Example 2:
+    intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+    newInterval = [4,8]
+    expected = [[1,2],[3,10],[12,16]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+
+     # Example 3
+    intervals = [[1,3], [5,7], [8,12]]
+    newInterval = [4,6]
+    expected = [[1,3], [4,7], [8,12]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+    
+    # Example 4
+    intervals = [[1,3], [5,7], [8,12]]
+    newInterval = [4,10]
+    expected = [[1,3], [4,12]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+    
+    # Example 5
+    intervals = [[2,3],[5,7]]
+    newInterval = [1,4]
+    expected = [[1,4], [5,7]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
+
+    # Example 3
+    intervals = [[1,2], [3,4], [5,6]]
+    newInterval = [2,5]
+    expected = [[1,6]]
+    output = InsertInterval(intervals, newInterval)
+    print(expected)
+    print(output)
+    print(expected == output)
+    print()
